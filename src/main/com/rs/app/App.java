64309821bcdec37a1ce1eb992df04f3b8818a912
dasras <rs.rashmidas@gmail.com>
2020-01-06 15:52:16 +0530

    updated App.java

--- src/main/com/rs/app/App.java
+++ src/main/com/rs/app/App.java
@@ -12,7 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
-        System.out.println("Hello19923... Hello");
+        System.out.println("Hello10023... Hello");
       }
 
     private final String getMessage() {
